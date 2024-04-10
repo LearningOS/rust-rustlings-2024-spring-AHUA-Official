c089e21b521f614bf5fcfe2a8da2f328d32ef042
ahua_official <cmkallj@163.com>
2024-04-10 22:36:38 +0800

    可莉

--- exercises/vecs/vecs1.rs
+++ exercises/vecs/vecs1.rs
@@ -2,16 +2,25 @@
 //
 // Your task is to create a `Vec` which holds the exact same elements as in the
 // array `a`.
-//
-// Make me compile and pass the test!
-//
+//创建一个 Vec（向量），其中包含与数组 a 中完全相同的元素
+//VEC是什么  Vec 是 Rust 标准库中的一种动态数组类型，它可以在运行时动态增长和缩减大小。
+//与数组不同，Vec 的
+//大小是可变的，它可以动态地增加或减少元素数量。因此，Vec 提供了更灵活的内存管理和更方便的数据操作方式。
+//le and pass the test!
+/*使用 Vec，你可以：
+
+动态地添加和移除元素。
+在堆上存储元素，因此可以容纳任意数量的元素（取决于可用内存）。
+使用 push() 方法在末尾添加元素，使用 pop() 方法从末尾移除元素。
+使用 len() 方法获取 Vec 中元素的数量。
+使用索引或迭代器访问 Vec 中的元素。 */
 // Execute `rustlings hint vecs1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM N
 
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
+    let v = Vec::from(a);// TODO: declare your vector here with the macro for vectors
 
     (a, v)
 }
--- exercises/vecs/vecs2.rs
+++ exercises/vecs/vecs2.rs
@@ -2,31 +2,40 @@
 //
 // A Vec of even numbers is given. Your task is to complete the loop so that
 // each number in the Vec is multiplied by 2.
-//
+//完成循环，使得给定的 Vec 中的每个数字都乘以 2。
 // Make me pass the test!
 //
 // Execute `rustlings hint vecs2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+/*
+考察 Vec 的迭代和修改：vec_loop 函数展示了如何使用 iter_mut 方法来遍历可变引用的 Vec，
+并通过乘以 2 修改每个元素的值。
+
+迭代器和函数式编程：vec_map 函数展示了如何使用 map 方法和闭包对 Vec 中的每个元素进行操作，
+而不需要修改原始 Vec。
+这种方法更符合函数式编程的风格，能够更简洁地表达意图。*/
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for element in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
-        // multiplied by 2.
-        ???
+        // multiplied by 2.  解引用运算符（*）来访问每个元素的可变引用
+        *element  *=2;
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
     v
 }
+/*在第二个函数（vec_map）中，我们不需要使用解引用运算符，因为map函数期望闭包返回新值。这允许我们在不直接修改原始向量的情况下对每个元素进行转换。
 
+至于在Rust开发人员中更常用的模式，这取决于具体的用例和个人偏好。
+
+对于直接修改的情况，使用iter_mut更为明确，可能在需要清晰度的情况下更受欢迎。
+对于无需修改的转换，使用map更具功能性，可能在需要不可变性和可组合性的情况下更受欢迎。 */
+ 
 fn vec_map(v: &Vec<i32>) -> Vec<i32> {
-    v.iter().map(|element| {
-        // TODO: Do the same thing as above - but instead of mutating the
-        // Vec, you can just return the new number!
-        ???
-    }).collect()
+    v.iter().map(|element| element * 2).collect()
 }
+
 
 #[cfg(test)]
 mod tests {
